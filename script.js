7c441b102be5e26d7582578a9efad5bc0b45d077
BoobalanRP <116268868+BoobalanRP@users.noreply.github.com>
2023-08-15 11:57:05 +0530

    create file

---
+++ script.js
@@ -0,0 +1,23 @@
+const container = document.querySelector(".container");
+
+document.querySelector(".open-navbar-icon").addEventListener("click", () => {
+  container.classList.add("change");
+});
+
+document.querySelector(".close-navbar-icon").addEventListener("click", () => {
+  container.classList.remove("change");
+});
+
+const colors = ["#c6005db3", "#311c8fb3", "#ff9500dc", "#311c8fb3", "#c6005db3"];
+
+let i = 0;
+
+Array.from(document.querySelectorAll(".nav-link")).forEach(item => {
+  item.style.cssText = `background-color: ${colors[i++]}`;
+});
+
+Array.from(document.querySelectorAll(".navigation-button")).forEach(item => {
+  item.onclick = () => {
+    item.parentElement.parentElement.classList.toggle("change");
+  };
+});
